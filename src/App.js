efdad587856ae39fab704f2cf7bbe30fcf15d563
nathanwang <wang.p@husky.neu.edu>
2021-04-12 00:02:46 -0700

    finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import logo from './logo.svg';
+import React, { useState,useEffect } from "react";
 import './App.css';
+import {createFFmpeg,fetchFile} from '@ffmpeg/ffmpeg';
+ const ffmpeg=  createFFmpeg({log:true});
 
 function App() {
-  return (
+  const [ready,setReady] = useState(false);
+  const [video,setVideo] = useState();
+  const [gif,setGif] = useState();
+  const load = async ()=>{
+    await ffmpeg.load();
+    setReady(true); 
+  }
+
+  const convertToGif = async()=>{
+    setReady(false);
+    ffmpeg.FS('writeFile','test.mp4',await fetchFile(video));
+
+    await ffmpeg.run('-i','test.mp4','-t','5.0','-ss','2.0','-f','gif','out.gif');
+    const data = ffmpeg.FS('readFile','out.gif');
+    const url = URL.createObjectURL(new Blob([data.buffer],{type:'image/gif'}));
+    setGif(url);
+    setReady(true);
+  }
+
+  useEffect(() => {
+    load();
+  }, [])
+  return ready ? (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CS5610 Demo Purpose</h1>
+      <h3>Only generate 5 secons of gif, with 2 seconds cut off at beginning of the video</h3>
+      {video &&<video
+      controls
+      width="250"
+      src={URL.createObjectURL(video)}>
+        </video>}
+     <input type = "file" onChange={(e) => setVideo(e.target.files?.item(0))}/>
+     <h3>Result</h3>
+     <button onClick={convertToGif}>Convert</button>
+     {gif && <img src = {gif} width = "250"/>}
     </div>
+  ):(
+    <h1>Loading / Converting</h1>
   );
 }
 
